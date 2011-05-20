75eeacb21cbf66fbd2ec0b180119871989aec51f
talpa <talpa@0d6777d6-8b17-4ca8-b775-d3f1221f2db1>
2011-05-20 08:09:38 +0000

    Added some initalt support files
    git-svn-id: svn+ssh://svn.talpa.dk/home/svn/minimus32@1 0d6777d6-8b17-4ca8-b775-d3f1221f2db1

---
+++ localbuild.pl
@@ -0,0 +1,12 @@
+$lazyLinking = 1;
+
+$compiler = "avr-gcc";
+$codeSuffix = "c";
+$includeSuffix = "h|c";
+$cflags = "-mmcu=atmega32u2 -c -I/usr/lib/avr/include ";
+
+
+$linker = $compiler;
+$ldflags = "-mmcu=atmega32u2 ";
+
+$testProgram = "./upload_and_test.sh ";
---
+++ upload_and_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+avr-objcopy -j .text -j .data -O ihex $1 $1.hex
+sudo  dfu-programmer atmega32u2 erase
+sudo  dfu-programmer atmega32u2 flash $1.hex
+sudo  dfu-programmer atmega32u2 reset
+sudo  dfu-programmer atmega32u2 reset
