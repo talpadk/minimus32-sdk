361ad32cad3c886d20f8cec046ff49a64f12b59e
smet <smet@0d6777d6-8b17-4ca8-b775-d3f1221f2db1>
2011-06-04 20:43:28 +0000

    deleted borked commit @mbs
    git-svn-id: svn+ssh://svn.talpa.dk/home/svn/minimus32@29 0d6777d6-8b17-4ca8-b775-d3f1221f2db1

--- src/programs/windspeed/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ]; then
-	echo "Usage: $0 <file.hex>"
-	exit 1
-fi
-echo "Uploading '$1' to Minimus USB"
-dfu-programmer atmega32u2 erase && dfu-programmer atmega32u2 flash "$1"
