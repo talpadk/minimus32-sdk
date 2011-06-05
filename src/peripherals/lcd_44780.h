ce0ca4be92143c79fc03966664afdecd52781921
talpa <talpa@0d6777d6-8b17-4ca8-b775-d3f1221f2db1>
2011-06-05 15:59:52 +0000

    Updated the LCD documentation
    git-svn-id: svn+ssh://svn.talpa.dk/home/svn/minimus32@38 0d6777d6-8b17-4ca8-b775-d3f1221f2db1

--- src/peripherals/lcd_44780.h
+++ src/peripherals/lcd_44780.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+///A command that moves the cursor to a different position, the offset is added to the command, the offset depends on the size of the display line 2 on a 16x2 display is LCD_44780_GOTO_CMD+64
 #define LCD_44780_GOTO_CMD (0x80)
 
 /** 
@@ -18,9 +19,26 @@ void lcd_44780_init();
  */
 void lcd_44780_power_up_delay();
 
+/** 
+ * Sends a command byte to the display
+ * 
+ * @param command the byte to send
+ */
 void lcd_44780_command(const uint8_t command);
 
+/** 
+ * Prints a null terminated string to the current cursor position
+ * 
+ * @param string the string to print
+ */
 void lcd_44780_print(const char * string);
+
+/** 
+ * Sends a byte to the display, the byte may be a command or a data byte
+ * 
+ * @param data the byte to send
+ * @param is_command if true send the byte as a command else send it as data
+ */
 void lcd_44780_write_byte(uint8_t data, char is_command);
 
 
