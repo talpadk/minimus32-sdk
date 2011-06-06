08cd1babc0abdeb6c0c3d3e1370acaee2905a019
smet <smet@0d6777d6-8b17-4ca8-b775-d3f1221f2db1>
2011-06-06 18:32:54 +0000

    timer based debounce
    git-svn-id: svn+ssh://svn.talpa.dk/home/svn/minimus32@41 0d6777d6-8b17-4ca8-b775-d3f1221f2db1

--- src/programs/windspeed/windspeed.c
+++ src/programs/windspeed/windspeed.c
@@ -10,7 +10,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/atomic.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 #include "minimus32.h"
 #include "watchdog.h"
@@ -20,15 +20,20 @@
 #include "lcd_44780.h"
 
 int count = 0;
+timer1_callback reenable_interrupt_7_callback;
 
 void print_count(void *data) {
-	char str_count[33];
+	char str_count[16];
 	ATOMIC_BLOCK(ATOMIC_FORCEON) {
-		itoa(count, str_count, 10);
-		count = 0;
+		sprintf(str_count, "%16d", count);
+//		count = 0;
 	}
 	lcd_44780_command(LCD_44780_GOTO_CMD+64);
 	lcd_44780_print(str_count);
+}
+
+void reenable_interrupt_7(void *data) {
+	enable_external_interrupt_input(7, rising);
 }
 
 int main(void) {
@@ -43,8 +48,8 @@ int main(void) {
 	// Print "headline"
 	lcd_44780_print("1 sec. count:");
 	
-	timer1_callback print_count_callback;
 	timer1_clock_init();
+	timer1_callback print_count_callback;
 	timer1_clock_register_callback(1, 0, 1, &print_count, 0, &print_count_callback);
 
 	while (1) {
@@ -52,8 +57,12 @@ int main(void) {
 	return 0;
 }
 
+// Shortcut PD7 and GND to trigger INT7
 ISR(INT7_vect) {
-	// Shortcut PD7 and GND to trigger INT7
+	// Disable the interrupt, and set timer to enable interrupt in X ms
+	disable_external_interrupt_input(7);
+	timer1_clock_register_callback(0, 20, 0, &reenable_interrupt_7, 0, &reenable_interrupt_7_callback);
+	// Count up
 	ATOMIC_BLOCK(ATOMIC_FORCEON) {
 		count++;
 	}
