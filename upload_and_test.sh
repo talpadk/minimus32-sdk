70b191522ef0a22f692c81b7372144979c602265
smet <smet@0d6777d6-8b17-4ca8-b775-d3f1221f2db1>
2011-06-11 17:35:46 +0000

    switched 2 resets into 1 start
    git-svn-id: svn+ssh://svn.talpa.dk/home/svn/minimus32@46 0d6777d6-8b17-4ca8-b775-d3f1221f2db1

--- upload_and_test.sh
+++ upload_and_test.sh
@@ -3,5 +3,4 @@
 avr-objcopy -j .text -j .data -O ihex $1 $1.hex
 dfu-programmer atmega32u2 erase
 dfu-programmer atmega32u2 flash $1.hex
-dfu-programmer atmega32u2 reset
-dfu-programmer atmega32u2 reset
+dfu-programmer atmega32u2 start
