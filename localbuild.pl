c87034a378c6d0e50da89d19637faabcb60dea82
talpa <talpa@0d6777d6-8b17-4ca8-b775-d3f1221f2db1>
2012-10-28 08:54:58 +0000

    Added build colours Relaxed the -clean user feedback Added optimization to the gcc arguments
    git-svn-id: svn+ssh://svn.talpa.dk/home/svn/minimus32@148 0d6777d6-8b17-4ca8-b775-d3f1221f2db1

--- localbuild.pl
+++ localbuild.pl
@@ -1,10 +1,13 @@
 $lazyLinking = 1;
+$useColours = 1;
+$cleanConfirm = 0;
+
 
 $compiler = "avr-gcc";
 $codeSuffix = "c";
 $includeSuffix = "h|c";
 $stripIncludePath = 1;
-$cflags = "-mmcu=atmega32u2 -D F_CLOCK=16000000 -D F_CPU=16000000 -Wall -c -I/usr/lib/avr/include ";
+$cflags = "-mmcu=atmega32u2 -O3 -D F_CLOCK=16000000 -D F_CPU=16000000 -Wall -c -I/usr/lib/avr/include ";
 
 $cflags .= '-std=gnu99 -D MCU=ATMEGA32U2 -D BOARD=NONE -D USB_DEVICE_ONLY -D FIXED_CONTROL_ENDPOINT_SIZE=8 -D FIXED_NUM_CONFIGURATIONS=1 -D USE_FLASH_DESCRIPTORS -D USE_STATIC_OPTIONS="(USB_DEVICE_OPT_FULLSPEED | USB_OPT_REG_ENABLED | USB_OPT_AUTO_PLL)" -D INTERRUPT_CONTROL_ENDPOINT ';
 
