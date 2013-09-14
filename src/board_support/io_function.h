682da85b5d80ee987d11cfadbc5e8d33333dff69
Visti Andresen <talpa@talpa.dk>
2013-09-14 16:26:12 +0200

    Some readbility defines and comment updates

--- src/board_support/io_function.h
+++ src/board_support/io_function.h
@@ -1,6 +1,14 @@
 #ifndef MOD_IO_FUNCTION_H
 #define MOD_IO_FUNCTION_H
 
+///For use with a io_outFunction, sets the pin high when the pin is configured as an output
+/**@see io_setOutput
+ */
+#define io_PIN_HIGH (255)
+///For use with a io_outFunction, sets the pin low when the pin is configured as an output
+/**@see io_setOutput
+ */
+#define io_PIN_LOW  (0)
 /** 
  * A prototype for a IO function used for creating pointers to functions handling bit IO output
  * 
@@ -15,11 +23,16 @@ typedef void(*io_outFunction)(unsigned char value);
  */
 typedef unsigned char(*io_inFunction)(void);
 
+///For use with a io_setOutput function, defines the pin as a output
+#define io_OUTPUT (255)
+
+///For use with a io_setOutput function, defines the pin as a input
+#define io_INPUT  (0)
 /** 
- * Sets the pin as an output.
- * If it is input it will be hi-z unless the pin has been: Set high and PUD is not set.
+ * Sets the pin as an output or input pin.
+ * If it is an input pin it will be hi-z unless the pin has been: Set high using io_outFunction and the PUD bit is not set.
  * 
- * PUD is the global pull up disable, the reset conition is unset.
+ * The PUD bit is the global pull up disable, the reset conition is unset.
  * 
  * @param value 255 to set as output, 0 for input
  * 
