81ace39e97ecc9e0ea6cd92f808db1f7f738523e
Visti Andresen <talpa@talpa.dk>
2014-12-04 21:57:18 +0100

    Changes to support multi target building

--- localbuild.pl
+++ localbuild.pl
@@ -1,21 +1,24 @@
 $lazyLinking = 1;
 $useColours = 1;
 $cleanConfirm = 0;
+$newTargetClean = 1;
+
+#$showCompilerCommand = 1;
 
 
 $compiler = "avr-gcc";
 $codeSuffix = "c";
 $includeSuffix = "h|c";
 $stripIncludePath = 1;
-$cflags = "-mmcu=atmega32u2 -O3 -D F_CLOCK=16000000 -D F_CPU=16000000 -Wall -c -I/usr/lib/avr/include ";
+$cflags = "-O3 -D F_CLOCK=16000000 -D F_CPU=16000000 -Wall -c -I/usr/lib/avr/include ";
 
-$cflags .= '-std=gnu99 -D MCU=ATMEGA32U2 -D BOARD=NONE -D USB_DEVICE_ONLY -D FIXED_CONTROL_ENDPOINT_SIZE=8 -D FIXED_NUM_CONFIGURATIONS=1 -D USE_FLASH_DESCRIPTORS -D USE_STATIC_OPTIONS="(USB_DEVICE_OPT_FULLSPEED | USB_OPT_REG_ENABLED | USB_OPT_AUTO_PLL)" -D INTERRUPT_CONTROL_ENDPOINT ';
+$cflags .= '-std=gnu99 -D BOARD=NONE -D USB_DEVICE_ONLY -D FIXED_CONTROL_ENDPOINT_SIZE=8 -D FIXED_NUM_CONFIGURATIONS=1 -D USE_FLASH_DESCRIPTORS -D USE_STATIC_OPTIONS="(USB_DEVICE_OPT_FULLSPEED | USB_OPT_REG_ENABLED | USB_OPT_AUTO_PLL)" -D INTERRUPT_CONTROL_ENDPOINT ';
 
 #LUFA settings
 $cflags .= '-D ARCH=ARCH_AVR8 -D F_USB=16000000 '; 
 
 $linker = $compiler;
-$ldflags = "-mmcu=atmega32u2 -O3 ";
+$ldflags = "-O3 ";
 
 $testProgram = "./upload_and_test.sh ";
 
@@ -36,5 +39,20 @@ sub autoFunction
   chdir($workingDir);
 
 }
-
 $autoProcessingRef = \&autoFunction;
+
+#Set overwritable defines
+sub beforeCompileRunFunction
+{
+	#handle mcu choice
+	if ($cflags =~ /-D\s*MCU=([^\s]+)/){
+		my $mcu = lc($1);
+		$cflags .= "-mmcu=$mcu ";
+		$ldflags .= "-mmcu=$mcu ";
+	}
+	else {
+		$cflags .= "-D MCU=ATMEGA32U2 -mmcu=atmega32u2 ";
+		$ldflags .= "-mmcu=atmega32u2 ";
+	}
+}
+$beforeCompileRunRef = \&beforeCompileRunFunction;
