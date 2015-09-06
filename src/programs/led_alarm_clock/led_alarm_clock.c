e9529a4dfa7e117c99cb942f65e1eb5b58915734
Visti Andresen <talpa@talpa.dk>
2015-09-06 09:29:19 +0200

    Controlling LED brightness using PWM works including 0 setting, code still needs cleanup

--- src/programs/led_alarm_clock/led_alarm_clock.c
+++ src/programs/led_alarm_clock/led_alarm_clock.c
@@ -21,21 +21,22 @@
 #include "adc_single_ended.h"
 #include "int_print.h"
 #include "sw_dither.h"
-#include "timer0_fast_timer.h"
+//#include "timer0_fast_timer.h"
 #include "timer1_clock.h"
 
 sw_dither blueLed;
 uint8_t blueLedOn;
 
+uint32_t current_=0;
 
-//Brightness control the LED
-ISR(TIMER0_COMPA_vect) {
-  if(sw_dither_animate(&blueLed)){
-    PORTB |= 0b00100000;
-  }
-  else {
-    PORTB &= 0b11011111;
-  }
+
+ISR(TIMER0_COMPB_vect) {
+  //current_=1000;
+  /*  if (!adc_is_busy()){
+    current_ = adc_get_result();
+    PORTB ^= 0b00100000;
+    adc_start_conversion();
+    }*/
 }
 
 void ledAnimate(void *data){
@@ -49,20 +50,46 @@ void ledAnimate(void *data){
   }
 }
 
+
+uint16_t ledPWM_ = 0;
+
+void setLedPWM(uint16_t setPoint){
+  ledPWM_ = setPoint;
+  if (setPoint>0){
+    TCCR0A = 0b10000011; //Timer 0 A output fast pwm
+    TCCR0B = 1; //no prescaling
+    OCR0A =  (setPoint-1); //Power
+    OCR0B =  190; //cur sample   
+  }
+  else {
+    TCCR0A = 0b00000011; //OC0A normal port operation
+    PORTD &= ~0b01000000; //turn it off (probably not needed)
+  }
+}
+
 int main(void) {
+  char inByte;
+  uint32_t adcValue;
   uint32_t supplyVoltage;
-	char buffer[UINT16_PRINT_DECIMAL_NULL_SIZE];
+  uint32_t printValue;
+  char buffer[UINT16_PRINT_DECIMAL_NULL_SIZE];
   timer1_callback ledTimer;
   
-	watchdog_disable();
-	clock_prescale_none();	
-	
-	PORTB &= 0b00000000; //turn off led
-	DDRB  |= 0b00100000; //enable led as output
-  sw_dither_init(&blueLed, 1023);
+  watchdog_disable();
+  clock_prescale_none();	
   
-  //3.91 kHz interrupt for LED control 
-  timer0_fast_timer_init(TIMER0_256, 16);
+  PORTB &= 0b00000000; //turn off led
+  
+  DDRB  |= 0b00100000; //enable led as output
+  sw_dither_init(&blueLed, 1023);
+
+
+  PORTD = 0; //power the LED switch off
+  DDRD |= 0b01000000; //OC0A as output
+
+  setLedPWM(0);
+  //TIMSK0 = 1<<OCIE0B;
+  
   
   timer1_clock_init();
   timer1_clock_register_callback (0, 500, 1, &ledAnimate, 0, &ledTimer);
@@ -70,30 +97,80 @@ int main(void) {
 
   
 	
-	async_serial_0_init(SERIAL_SPEED_9600);
+  async_serial_0_init(SERIAL_SPEED_9600);
 
-	adc_set_reference_and_adj(ADC_REF_AVCC, 0);
-	adc_set_channel(ADC0);
-	adc_set_speed_and_enable(ADC_DIV_128, 0, 0);
-
-	sei(); //global int enable
-
-	async_serial_0_write_string(VT100_CURSOR_OFF);
-	async_serial_0_write_string(VT100_CLEAR_SCREEN);
+  //ADCSRB |= 1<<ACME;
+  //adc_set_reference_and_adj(ADC_REF_1V1, 0);
+  adc_set_reference_and_adj(ADC_REF_AVCC, 0);
+  adc_set_channel(ADC1);
+  DIDR0 = 3;
+  //128kHZ @ 16MHZ
+  adc_set_speed_and_enable(ADC_DIV_128, 0, 0);
+  
+  sei(); //global int enable
+  
+  async_serial_0_write_string(VT100_CURSOR_OFF);
+  async_serial_0_write_string(VT100_CLEAR_SCREEN);
   while (1){
-		adc_start_conversion();
-		while(adc_is_busy()){}
-    //1024=5V from a 100k + 220k voltage divider  
-		supplyVoltage = adc_get_result();
-    supplyVoltage = supplyVoltage*1600/1024;
-    uint16PrintDecimalNull(supplyVoltage, 2, buffer);
+
+    
+    //PORTB ^= 0b00100000;
+    
+    if (async_serial_0_byte_ready()){
+      inByte = async_serial_0_read_byte();
+      if (inByte=='+' && ledPWM_<255){
+	setLedPWM(ledPWM_+1);
+      }
+      else if (inByte=='-' && ledPWM_>0){
+	setLedPWM(ledPWM_-1);
+      }
+      
+    }
+
+    adc_set_channel(ADC0);
+    adc_start_conversion();
+    while(adc_is_busy()){}
+    adcValue = adc_get_result();
+    //supplyVoltage = ((supplyVoltage*31)+adcValue)/32;
+    supplyVoltage = adcValue;
+    
+    //adc_set_channel(ADC_GND);
+    adc_set_channel(ADC1);
+    adc_start_conversion();
+    while(adc_is_busy()){}
+    adcValue = adc_get_result();
+    //current_ = ((current_*31)+adcValue)/32;
+    current_ = adcValue;
+    
+    async_serial_0_write_string(VT100_CURSOR_HOME);
+    async_serial_0_write_string("=== Hello world ===\r\n");
+
+    
+    //1024=5V from a 100k + 220k voltage divider
+    printValue = supplyVoltage*1600/1024;
+    uint16PrintDecimalNull(printValue, 2, buffer);
     replaceLeadingZeros(buffer);
+    async_serial_0_write_string(buffer);
+    async_serial_0_write_string(" V\r\n");
 
-
-		async_serial_0_write_string(VT100_CURSOR_HOME);
-		async_serial_0_write_string("=== Hello world ===\r\n");
-		async_serial_0_write_string(buffer);
-    async_serial_0_write_string("V\r\n");
+    //1024=5V 0.1 Ohm resistor and 19 times gain
+    //5V = 2631.57 mA @0.1 Ohm
+    //1V1 = 0.5789 @0.1 Ohm
+    //5V = 887.19 mA @ 0.3 Ohm
+    printValue = current_;
+    //printValue = printValue*579/1024; //1.1V reference
+    printValue = printValue*887/1024; //5V reference
+    uint16Print(printValue, buffer);
+    buffer[UINT16_PRINT_SIZE]=0;
+    replaceLeadingZeros(buffer);
+    async_serial_0_write_string(buffer);
+    async_serial_0_write_string(" mA\r\n");
+    
+    uint16Print(ledPWM_, buffer);
+    buffer[UINT16_PRINT_SIZE]=0;
+    replaceLeadingZeros(buffer);
+    async_serial_0_write_string(buffer);
+    async_serial_0_write_string(" PWM\r\n");
   }
   return 0;
 }
