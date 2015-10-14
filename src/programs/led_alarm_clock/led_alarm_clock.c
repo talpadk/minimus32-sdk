c066726c50841f87ea808a43a5fe736db53a2d77
Visti Andresen <talpa@talpa.dk>
2015-10-14 15:36:05 +0200

    Added a graphical LCD to the led_alarm_clock

--- src/programs/led_alarm_clock/led_alarm_clock.c
+++ src/programs/led_alarm_clock/led_alarm_clock.c
@@ -9,45 +9,47 @@
  * @ingroup prog
  */
 
+/*
+ * PC0 = N.C. (Used to be battery voltage)
+ * PC1 = LED current signal
+ * PC2 = LED temperature (from LM35)
+ * PC3 = LED voltage
+ *
+ * PD6 = LED PWM (OC0A)
+ * 
+ * PB2 = LCD CS
+ * PB3 = LCD MOSI
+ * PB4 = LCD MISO
+ * PB5 = LCD SCK
+ * PD2 = LDC D/C
+ * PD4 = LDC RESET
+ */
+
+
 
 #include <avr/io.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
 #include "watchdog.h"
 #include "sys_clock.h"
 #include "vt100_codes.h"
 #include "async_serial_0.h"
 #include "int_print.h"
-#include "sw_dither.h"
 //#include "timer0_fast_timer.h"
 #include "timer1_clock.h"
 #include "led_alarm_clock_adc.h"
+#include "twi.h"
+#include "lcd_ili9341.h"
 
-sw_dither blueLed;
 uint8_t blueLedOn;
 
-#define PANIC_LED_CURRENT (700)
+//550mA (550*1024/579)
+#define PANIC_LED_RAW_CURRENT (973)
 
 
 ISR(TIMER0_COMPB_vect) {
-  //current_=1000;
-  /*  if (!adc_is_busy()){
-    current_ = adc_get_result();
-    PORTB ^= 0b00100000;
-    adc_start_conversion();
-    }*/
-}
-
-void ledAnimate(void *data){
-  if(blueLedOn){
-    blueLedOn = 0;
-    sw_dither_set(&blueLed, 0);
-  }
-  else {
-    blueLedOn = 1;
-    sw_dither_set(&blueLed, 10);
-  }
 }
 
 int16_t ledCurrentSP_ = 0;
@@ -67,9 +69,25 @@ void setLedPWM(uint16_t setPoint){
   }
 }
 
+void setDataCommandPin(unsigned char value){
+  value &= 0b00000100;
+  PORTD = (PORTD & 0b11111011)|value;
+}
+
+void setChipSelectPin(unsigned char value){
+  value &= 0b00000100;
+  PORTB = (PORTB & 0b11111011)|value;
+}
+
+
+lcd_ili9341_device display = {
+  &setChipSelectPin,
+  &setDataCommandPin
+};
+
 
 void regulate(void *data){
-  int16_t spBandSize = ledCurrentSP_/10;
+  int16_t spBandSize = ledCurrentSP_/20;
   int16_t ledCurrent = getLEDCurrent();
 
   if (spBandSize<2) { spBandSize=2; }
@@ -88,8 +106,8 @@ void regulate(void *data){
   setLedPWM(ledPWM_);
 }
 
-void ledCurrentCallback(uint16_t current){
-  if (current>PANIC_LED_CURRENT){
+void ledCurrentCallback(uint16_t rawCurrent){
+  if (rawCurrent>PANIC_LED_RAW_CURRENT){
     setLedPWM(0);
   }
 }
@@ -97,33 +115,39 @@ void ledCurrentCallback(uint16_t current){
 int main(void) {
   char inByte;
   char buffer[UINT16_PRINT_DECIMAL_NULL_SIZE];
-  timer1_callback ledTimer;
   timer1_callback regulateTimer;
   
   watchdog_disable();
   clock_prescale_none();	
+  spi_config_io_for_master_mode();
   
-  PORTB &= 0b00000000; //turn off led
+  DDRD  |= 0b01010100; //PD6, PD4,PD2 as output
+
+  PORTD &=~0b00010000; //reset LCD
+  _delay_ms(10);
+  PORTD |= 0b00010000;
+  _delay_ms(100);
   
-  DDRB  |= 0b00100000; //enable led as output
-  sw_dither_init(&blueLed, 1023);
 
-
-  PORTD = 0; //power the LED switch off
-  DDRD |= 0b01000000; //OC0A as output
 
   setLedPWM(0);
   
-  
+
   timer1_clock_init();
-  timer1_clock_register_callback (0, 500, 1, &ledAnimate, 0, &ledTimer);
   timer1_clock_register_callback (0, 100, 1, &regulate, 0, &regulateTimer);
   initADCSystem();
   registerCurrentCallback(&ledCurrentCallback);
 	
   async_serial_0_init(SERIAL_SPEED_9600);
-  
+ 
   sei(); //global int enable
+
+
+  lcd_ili9341_obtainBus(1);
+
+  lcd_ili9341_selectDevice(&display);
+  lcd_ili9341_init();
+  lcd_ili9341_drawFilledRectangle(ILI9341_COLOUR_GREEN, 0, 239, 0, 319);
   
   async_serial_0_write_string(VT100_CURSOR_OFF);
   async_serial_0_write_string(VT100_CLEAR_SCREEN);
@@ -131,7 +155,7 @@ int main(void) {
   while (1){
     if (async_serial_0_byte_ready()){
       inByte = async_serial_0_read_byte();
-      if (inByte=='+' && ledCurrentSP_<600){
+      if (inByte=='+' && ledCurrentSP_<500){
 	ledCurrentSP_+=10;
       }
       else if (inByte=='-' && ledCurrentSP_>0){
